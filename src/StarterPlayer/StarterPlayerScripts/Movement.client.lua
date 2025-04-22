6bc0148a7682b5aa6a8c1a1a3bd6b5d7159081b3
Christian Toney <toney1c@cmich.edu>
2025-04-22 19:37:04 -0400

    re-add movement

--- src/StarterPlayer/StarterPlayerScripts/Movement.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Movement.client.lua
@@ -1,30 +1,20 @@
 --!strict
+local UserInputService = game:GetService("UserInputService");
 local ContextActionService = game:GetService("ContextActionService");
 
-local vector = Vector3.zero;
 local function moveCharacter(actionName, inputState)
 
 	local force = 30;
 
-	if actionName == "MoveLeft" then
-
-		vector = Vector3.new(vector.X, vector.Y, if inputState == Enum.UserInputState.Begin then force else 0);
-
-	elseif actionName == "MoveRight" then
-
-		vector = Vector3.new(vector.X, vector.Y, if inputState == Enum.UserInputState.Begin then -force else 0);
-
-	elseif actionName == "MoveForward" then
-
-		vector = Vector3.new(if inputState == Enum.UserInputState.Begin then force else 0, vector.Y, vector.Z);
-
-	elseif actionName == "MoveBackward" then
-
-		vector = Vector3.new(if inputState == Enum.UserInputState.Begin then -force else 0, vector.Y, vector.Z);
-
-	end
-
-	workspace.CousinRicky.PlayerCharacter.LinearVelocity.VectorVelocity = vector;
+	local shouldMoveLeft = UserInputService:IsKeyDown(Enum.KeyCode.Left) or UserInputService:IsKeyDown(Enum.KeyCode.A);
+	local shouldMoveRight = UserInputService:IsKeyDown(Enum.KeyCode.Right) or UserInputService:IsKeyDown(Enum.KeyCode.D);
+	local shouldMoveForward = UserInputService:IsKeyDown(Enum.KeyCode.Up) or UserInputService:IsKeyDown(Enum.KeyCode.W);
+	local shouldMoveBackward = UserInputService:IsKeyDown(Enum.KeyCode.Down) or UserInputService:IsKeyDown(Enum.KeyCode.S);
+	workspace.CousinRicky.PlayerCharacter.LinearVelocity.VectorVelocity = Vector3.new(
+		if shouldMoveForward and shouldMoveBackward then 0 elseif shouldMoveBackward then force elseif shouldMoveForward then -force else 0, 
+		0, 
+		if shouldMoveLeft and shouldMoveRight then 0 elseif shouldMoveLeft then force elseif shouldMoveRight then -force else 0
+	);
 
 end
 
