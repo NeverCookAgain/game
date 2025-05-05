539e1057d7ac2188ef6ccaa60f6ae94c39369c23
Christian Toney <toney1c@cmich.edu>
2025-05-05 18:20:55 -0400

    add inventory pointer: hotkeys

--- src/StarterPlayer/StarterPlayerScripts/InventoryScript/InventoryContainer/init.lua
+++ src/StarterPlayer/StarterPlayerScripts/InventoryScript/InventoryContainer/init.lua
@@ -1,5 +1,6 @@
 --!strict
 
+local ContextActionService = game:GetService("ContextActionService");
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 
 local React = require(ReplicatedStorage.Shared.Packages.react);
@@ -67,6 +68,63 @@ local function InventoryContainer(properties: InventoryContainerProperties)
     end;
 
   end, {});
+
+  React.useEffect(function()
+  
+    local function onHotkeyInput(actionName: string, inputState: Enum.UserInputState, inputObject: InputObject)
+
+      local map = {
+        [Enum.KeyCode.One] = 1;
+        [Enum.KeyCode.Two] = 2;
+        [Enum.KeyCode.Three] = 3;
+        [Enum.KeyCode.Four] = 4;
+        [Enum.KeyCode.Five] = 5;
+        [Enum.KeyCode.Six] = 6;
+        [Enum.KeyCode.Seven] = 7;
+        [Enum.KeyCode.Eight] = 8;
+        [Enum.KeyCode.Nine] = 9;
+        [Enum.KeyCode.KeypadOne] = 1;
+        [Enum.KeyCode.KeypadTwo] = 2;
+        [Enum.KeyCode.KeypadThree] = 3;
+        [Enum.KeyCode.KeypadFour] = 4;
+        [Enum.KeyCode.KeypadFive] = 5;
+        [Enum.KeyCode.KeypadSix] = 6;
+        [Enum.KeyCode.KeypadSeven] = 7;
+        [Enum.KeyCode.KeypadEight] = 8;
+        [Enum.KeyCode.KeypadNine] = 9;
+      }
+
+      local newSelectedSlot = map[inputObject.KeyCode];
+
+      if inputState == Enum.UserInputState.Begin and newSelectedSlot then
+
+        local item = inventory[newSelectedSlot];
+        if item then
+
+          setSelectedItemSlot(newSelectedSlot);
+
+        end;
+
+      end;
+
+    end;
+
+    ContextActionService:BindAction(
+      "InventoryHotkey", onHotkeyInput, false, Enum.KeyCode.One, Enum.KeyCode.Two, 
+      Enum.KeyCode.Three, Enum.KeyCode.Four, Enum.KeyCode.Five, Enum.KeyCode.Six, 
+      Enum.KeyCode.Seven, Enum.KeyCode.Eight, Enum.KeyCode.Nine, Enum.KeyCode.KeypadOne, 
+      Enum.KeyCode.KeypadTwo, Enum.KeyCode.KeypadThree, Enum.KeyCode.KeypadFour, 
+      Enum.KeyCode.KeypadFive, Enum.KeyCode.KeypadSix, Enum.KeyCode.KeypadSeven, 
+      Enum.KeyCode.KeypadEight, Enum.KeyCode.KeypadNine
+    );
+
+    return function()
+
+      ContextActionService:UnbindAction("InventoryHotkey");
+
+    end;
+
+  end, {inventory});
 
   React.useEffect(function()
   
